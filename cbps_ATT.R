303d971d39dfce0d21487cb790b40487aa641402
apoorvalal <apoorval@netflix.com>
2024-01-16 15:45:27 -0800

    "first push"

---
+++ cbps_ATT.R
@@ -0,0 +1,128 @@
+library(ebal)
+# Covariate Balancing Propensity Score
+# This simple script uses Base R's `optim` to solve a variant of (7.10) from
+# http://web.stanford.edu/~swager/stats361.pdf to target ATT weights.
+# (see https://arxiv.org/abs/1601.05890 for details)
+# Input:
+#   X: nXp numeric covariate matrix
+#   W: binary treatment assignment vector
+#   intercept: whether to include an intercept in logistic model, default is TRUE.
+#   θ.init: optional starting values for theta.
+#   method: method argument passed to `optim`.
+#   control: control argument passed to `optim`.
+#   λ: optional ridge penalty (remember to scale X's appropriately if used)
+# Output:
+#   theta.hat: estimated thetas
+#   weights.0: IPW weights for control
+#   convergence: optim's convergence status. 0=success.
+#   balance condition: the LHS and RHS of the balance condition.
+
+cbps_att = function(
+    X, W,
+    intercept = TRUE,
+    theta.init = NULL, method = "BFGS",
+    control = list(), lam = NULL) {
+  if (!all(W %in% c(0, 1))) stop("W should be a binary vector.")
+  if (!is.numeric(X) || nrow(X) != length(W) || is.null(dim(X)) || anyNA(X)) {
+    stop("X should be a numeric matrix with nrows = length(W).")
+  }
+  .Xtheta = NULL
+  # ATT balance constraint is:
+  # 1/n1 \sum_{W_i = 0} e(x)/(1-e(x)) Xi = 1/n1 \sum_{Wi=1} Xi,
+  # which gives loss function
+  # (1 - W)exp(theta * X) - W * theta * X
+  .objective = \(θ, X, W0.idx, W1.idx, λ) {
+    .Xθ <<- X %*% θ
+    (sum(exp(.Xθ[W0.idx, ])) - sum(.Xθ[W1.idx, ])) / length(W1.idx) + sum(λ * θ^2)
+  }
+  .objective.gradient = \(θ, X0, Xsum1, W0.idx, n, λ) {
+    (colSums(X0 * exp(.Xθ[W0.idx, ])) - Xsum1) / n + 2 * λ * θ
+  }
+  if (is.null(lam)) λ = rep(0, ncol(X))
+  if (intercept) {
+    X = cbind(1, X)
+    λ = c(0, λ)
+  }
+  W1.idx = which(W == 1); W0.idx = which(W == 0)
+  if (is.null(theta.init)) {
+    # Use "naive" logistic starting values
+    idx.small = c(W1.idx, sample(W0.idx, length(W1.idx)))
+    glm = glm.fit(X[idx.small, ], W[idx.small], family = binomial())
+    θ.init = glm$coefficients
+    # update the intercept, (7) in https://gking.harvard.edu/files/0s.pdf
+    if (intercept) {
+      ρ = mean(W)
+      θ.init[1] = θ.init[1] - log((1 - ρ) / ρ) * length(idx.small) / sum(W)
+    }
+  }
+  X0 = X[W0.idx, ]
+  Xsum1 = colSums(X[W1.idx, ])
+  res = optim(
+    par = θ.init,
+    fn = \(z) .objective(z, X, W0.idx, W1.idx, λ),
+    gr = \(z) .objective.gradient(z, X0, Xsum1, W0.idx, nrow(X), λ),
+    method = method,
+    lower = -Inf,
+    upper = Inf,
+    control = control,
+    hessian = FALSE
+  )
+  θ.hat = res$par
+  weights.0 = exp(X %*% θ.hat)[, ]
+  LHS = colSums((1 - W) * X * weights.0) / sum(W == 1)
+  RHS = colSums(W * X) / sum(W == 1)
+  sd.W1 = apply(X[W1.idx, ], 2, sd)
+  sd.W1[sd.W1 == 0] = 1
+  sd.W = apply(X, 2, sd)
+  sd.W[sd.W == 0] = 1
+  mean.diff = colMeans(X[W1.idx, ]) -
+    apply(X[W0.idx, ], 2, \(x) weighted.mean(x, weights.0[W0.idx]))
+  balance.std = mean.diff / sd.W1
+  balance.std.pre = (colMeans(X[W1.idx, ]) - colMeans(X[W0.idx, ])) / sd.W1
+  balance.std.all = mean.diff / sd.W
+  balance.std.pre.all = (colMeans(X[W1.idx, ]) - colMeans(X[W0.idx, ])) / sd.W
+  list(
+    θ.hat = θ.hat,
+    weights.0 = weights.0,
+    convergence = res$convergence,
+    balance.condition = cbind(LHS = LHS, RHS = RHS),
+    balance.std = if (intercept) balance.std[-1] else balance.std,
+    balance.std.pre = if (intercept) balance.std.pre[-1] else balance.std.pre,
+    balance.std.all = if (intercept) balance.std.all[-1] else balance.std.all,
+    balance.std.pre.all = if (intercept) balance.std.pre.all[-1] else balance.std.pre.all
+  )
+}
+
+# %%
+n = 15000
+p = 40
+X = matrix(rnorm(n * p), n, p)
+W = rbinom(n, 1, 1 / (1 + exp(2.5 - X[, 1])))
+
+table(W)
+
+system.time(res <- cbps_att(X, W, control = list(trace = 10, maxit = 5000)))
+
+res$weights.0 %>% length
+# %%
+head(res$balance.condition)
+plot(res$balance.std)
+abline(h = 0)
+
+# %%
+data(lalonde.psid)
+df = setDT(lalonde.psid)
+yn = 're78'; wn = 'treat'; xn = setdiff(colnames(df), c(yn, wn))
+y = df[[yn]]; w = df[[wn]]; X = df[, ..xn] %>% as.matrix
+
+X %>% dim
+
+ebwt = ebalance(w, X)$w
+mean(y[w == 1]) - weighted.mean(y[w == 0], ebwt)
+# %%
+cbpswt = cbps_att(X, w)
+cbpw = cbpswt$weights.0[w == 0]
+mean(y[w == 1]) - weighted.mean(y[w == 0], cbpw)
+# %%
+cbpswt$θ.hat %>% length
+# %%
---
+++ cbps_torch.py
@@ -0,0 +1,57 @@
+# %%
+import numpy as np
+import pandas as pd
+
+import torch
+from torch.autograd import Variable
+
+from sklearn.preprocessing import MinMaxScaler
+
+
+# %%
+def cbps_att_torch(X, w, intercept=True, noi=False, niter=1000, lr=0.01):
+    n, p = X.shape
+    if intercept:
+        X = np.hstack((np.ones((n, 1)), X))
+    # preprocessing for numpy to pytorch
+    X, w = torch.from_numpy(X).float(), torch.from_numpy(w).float()
+    X, w = Variable(X), Variable(w)
+    # Parameters theta to be optimized
+    theta = Variable(torch.randn(p + 1), requires_grad=True)
+
+    # Define the loss function ℓ(θ) as per equation (7.10)
+    def loss_function(theta, X, W):
+        Xb = torch.matmul(X, theta)
+        loss = torch.mean(W * torch.exp(-Xb) + (1 - W) * Xb)
+        return loss
+
+    # Use an optimizer from PyTorch's optimization module, e.g., Adam
+    optimizer = torch.optim.Adam([theta], lr=lr)
+    # Iteratively apply gradient descent to minimize the loss function
+    for iteration in range(niter):
+        optimizer.zero_grad()  # Clear previous gradients
+        loss = loss_function(theta, X, w)  # Calculate loss
+        loss.backward()  # Compute gradients
+        optimizer.step()  # Update theta
+
+        if noi and iteration % 100 == 0:  # Print loss every 100 iterations
+            print(f"Iteration {iteration}: loss = {loss.item()}")
+    # Final theta value after optimization
+    theta_final = theta.data
+    return np.exp(X @ theta_final)[w == 0]
+
+
+# %%
+df = pd.read_csv("../df/lalonde_psid.csv")
+df.head()
+# %%
+w, y = df.treat.values, df.re78.values
+# naive estimate is garbage - true effect is ~ 1800
+y[w == 1].mean() - y[w == 0].mean()
+# %% stabilize by scaling covariates
+X = df.drop(columns=["treat", "re78"]).values
+X = MinMaxScaler().fit_transform(X)
+# %% cbps estimate - comes pretty close to true effect
+cbps_wt = cbps_att_torch(X, w, niter=5_000, lr=0.001, noi=False)
+y[w == 1].mean() - np.average(y[w == 0], weights=cbps_wt)
+# %%
