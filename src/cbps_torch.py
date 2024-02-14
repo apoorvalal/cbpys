f119abee11ab6ddf2e84724af895704dee6b821f
Ivan Higuera-Mendieta <bennytheball@fastmail.com>
2024-02-13 23:43:45 -0800

    Add dimension reduction via SVD
    We add some docs to the class, maybe something more technical would be
    better? Also, when X is big, might be convenient to reduce the space and
    factorize X to its minimal expression using a low-rank approach. I am
    hacking things here as I am picking the singular-value threshold myself,
    but there are basic alternatives (np.linalg.matrix_rank?), although
    inspired by LoRA, I wanted to try something more "aggressive".
    
    Bugs/TODO:
    ----
     - Maybe self.X can be defined via @property decorator instead
     - A logger might be a good idea

--- src/cbps_torch.py
+++ src/cbps_torch.py
@@ -19,24 +19,64 @@ class CBPS:
         noi=False,
         niter=1000,
         lr=0.01,
+        svd=None,
         reg=None,
     ):
-        """Covariate Balancing Propensity Score estimation in PyTorch."""
+        """Covariate Balancing Propensity Score estimation in PyTorch.
+
+        This class partially implements the covariate balancing propensity score
+        calculation for two different estimands: ATT and ATE. The class uses PyTorch
+        to optimize a taylored loss function and calculate iteratively optimal weights
+        to balance groups.
+
+        Some attempts at regularization are done during the optimization process.
+        First, we add a L1 regularization term to the loss function that penalizes
+        the loss using the absolute value of the weights. Second, we use SVD to reduce
+        the dimensionality of the input matrix. This is useful to reduce sensitivity
+        to noise and multicollinearity (especially when the number of covariates is
+        large).
+
+        Args:
+            X (np.array): covariate matrix
+            W (np.array): treatment dummy
+            estimand (str, optional): ATT or ATE. Defaults to "ATT".
+            intercept (bool, optional): Include intercept in model. Defaults to True.
+            noi (bool, optional): Print number of iterations. Defaults to False.
+            niter (int, optional): Number of iterations. Defaults to 1000.
+            lr (float, optional): Learning rate. Defaults to 0.01.
+            svd (int, optional): Dimensionality reduction using SVD. Defaults to None.
+            reg (float, optional): Regularization parameter. Defaults to None.
+        """
         self.estimand = estimand
         self.intercept = intercept
         self.noi = noi
         self.niter = niter
         self.lr = lr
         self.reg = reg
+        self.svd = svd
 
         if self.estimand not in ["ATT", "ATE"]:
             raise NotImplementedError("Estimand in ['ATT', 'ATE'] supported")
 
         # Define parameters for calculation
         self.n, self.p = X.shape
-
-        self.X = torch.from_numpy(X).float().to(device)
         self.W = torch.from_numpy(W).float().to(device)
+
+        # SVD for dimensionality reduction
+        if self.svd is not None:
+            X = torch.from_numpy(X).float().to(device)
+            U, S, V = torch.linalg.svd(X)
+            U_r = U[:, : self.svd]
+            S_r = torch.diag(S[: self.svd])
+            V_r = V[:, : self.svd].t()
+
+            self.X = U_r @ S_r @ V_r
+            print(f"Reduced dimensionality to {self.X.shape[1]} using SVD.")
+        else:
+            self.X = torch.from_numpy(X).float().to(device)
+
+        # Store losses when fit is called
+        self.loss = []
 
         if self.intercept:
             self.X = torch.concat((torch.ones((self.n, 1)).to(device), self.X), 1)
@@ -103,13 +143,21 @@ class CBPS:
             loss.backward()
             optimizer.step()
 
+            self.loss.append(loss.item())
+
             if self.noi and iteration % 100 == 0:
                 print(f"Iteration {iteration}: loss = {loss.item()}")
 
         return self.theta
 
     def weights(self):
-        """Return final weights after optimization."""
+        """Return final weights after optimization.
+
+        Compute weights for the estimand of interest.
+
+        Returns:
+            torch.tensor: weights
+        """
         weights = self.fit
         if self.estimand == "ATT":
             out = torch.exp(self.X @ weights)[self.W == 0]
